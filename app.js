3b58004474c6b06d734621c2884815ffa444d619
Alinoor Shukri <alinoorshukri7@gmail.com>
2020-02-06 21:22:08 +0300

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function initMap() {
     //map options
   var options = {
-      zoom: 8,
+      zoom: 14,
       center: {lat: -0.1675, lng: 35.9659}
     
   }
   //new map
   var map = new google.maps.Map(document.getElementById('map'),options);
-/*
+
 //add marker
-var marker = new google.maps.Marker({
+  var marker = new google.maps.Marker({
   position:{lat:-.3031, lng:36.08},
   map:map
 });
@@ -18,20 +18,20 @@ var infoWindow = new google.maps.infoWindow({
 });
 Marker.addlistener('click', function(){
   infoWindow.open(map,marker);
-});*/
+});
 
 //add marker function
-function addMarker(coords){
-  var marker = new google.maps.Marker({
-    position:props.coords,
-    map:map,
+//function addMarker(coords){
+ // var marker = new google.maps.Marker({
+  //  position:props.coords,
+  //  map:map,
     //icon:props.iconImage
-  });
-  if(props.iconImage){
+  //});
+  //if(props.iconImage){
     //set icon image
-    marker.setIcon(props.iconImage);
+    //marker.setIcon(props.iconImage);
   }
-  //check content
+  /*check content
   if(props.content){
     var infoWindow = new google.maps.infoWindow({
       content:props.content
@@ -42,8 +42,8 @@ function addMarker(coords){
 }
 }
 addMarker({coords:{lat:-.3031, lng:36.08},
-iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
 content:'<h1>Admin Block</h1>'});
-}
+*/
+//}
 
 
